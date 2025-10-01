4a188e3523ac89ee51e45cc09fd7e5258ced941f
jot083 <a20jonto@student.his.se>
2025-10-01 16:48:14 +0200

    Splines och skit

---
+++ Assets/Scripts/Audio Scripts/SplineMover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineMover : MonoBehaviour
+{
+    public Spline spline;
+    public Transform followObj;
+
+    private Transform thisTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisTransform.position = spline.WhereOnSpline(followObj.position);
+    }
+}
